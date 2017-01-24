eee7394667b34d6dd3d5a5b2317e3024e65b0490
cammls <camille.malis@coding-academy.fr>
2017-01-24 20:31:10 +0100

    with search

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -1,4 +1,7 @@
 class HomeController < ApplicationController
   def index
+    if params[:search]
+    @products = Product.search(params[:search])
+  end
   end
 end
--- app/models/product.rb
+++ app/models/product.rb
@@ -7,4 +7,11 @@ mount_uploader :picture, PictureUploader
   # validates :price, presence: true
   validates :price, :numericality => { :greater_than_or_equal_to => 0 }
   validates :category_id, presence: true
+
+  def self.search(search)
+  if search
+    # find(:all, :conditions => ['title LIKE ?', "%#{search}%"])
+    where(['title LIKE ?', "%#{search}%"])
+  end
+end
 end
